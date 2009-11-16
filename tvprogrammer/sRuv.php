7f6dca32a9d57ee3ad63353855ff5643946553f6
beggaboo85 <beggaboo85@b1fc508c-c7c7-11de-8410-e5b1e99fc78e>
2009-11-16 22:25:43 +0000

    dagatal með dagskrá Rúv og Stöð 2

--- tvprogrammer/sRuv.php
+++ tvprogrammer/sRuv.php
@@ -8,49 +8,49 @@
 			<table>
 				<tr class="header"><td><?php getWeekday(date('N')); ?></td></tr>
 				<tr class="header"><td> <?php echo date('d F');?> </td></tr>
-				<tr><td><?php getSkjarEinn($date); ?></td></tr>
+				<tr><td><?php getRuv($date); ?></td></tr>
 			</table>
 		</td>
 		<td class="day">
 			<table>
 				<tr class="header"><td><?php getWeekday(date('N')+1); ?></td></tr>
 				<tr class="header"><td> <?php echo date('d')+1 . ' ' . date('F');?> </td></tr>
-				<tr><td><?php getSkjarEinn(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+1, date("Y")))); ?></td></tr>
+				<tr><td><?php getRuv(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+1, date("Y")))); ?></td></tr>
 			</table>
 		</td>
 		<td class="day">
 			<table>
 				<tr class="header"><td><?php getWeekday(date('N')+2); ?></td></tr>
 				<tr class="header"><td> <?php echo date('d')+2 . ' ' . date('F');?> </td></tr>
-				<tr><td><?php getSkjarEinn(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+2, date("Y")))); ?></td></tr>
+				<tr><td><?php getRuv(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+2, date("Y")))); ?></td></tr>
 			</table>
 		</td>
 		<td class="day">
 			<table>
 				<tr class="header"><td><?php getWeekday(date('N')+3); ?></td></tr>
 				<tr class="header"><td> <?php echo date('d')+3 . ' ' . date('F')?> </td></tr>
-				<tr><td><?php getSkjarEinn(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+3, date("Y")))); ?></td></tr>
+				<tr><td><?php getRuv(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+3, date("Y")))); ?></td></tr>
 			</table>
 		</td>
 		<td class="day">
 			<table>
 				<tr class="header"><td><?php getWeekday(date('N')+4); ?></td></tr>
 				<tr class="header"><td> <?php echo date('d')+4 . ' ' . date('F');?> </td></tr>
-				<tr><td><?php getSkjarEinn(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+4, date("Y"))));?></td></tr>
+				<tr><td><?php getRuv(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+4, date("Y"))));?></td></tr>
 			</table>
 		</td>
 		<td class="day">
 			<table>
 				<tr class="header"><td><?php getWeekday(date('N')+5); ?></td></tr>
 				<tr class="header"><td> <?php echo date('d')+5 . ' ' . date('F');?> </td></tr>
-				<tr><td><?php getSkjarEinn(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+5, date("Y")))); ?></td></tr>
+				<tr><td><?php getRuv(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+5, date("Y")))); ?></td></tr>
 		</table>
 		</td>
 		<td class="day">
 			<table>
 				<tr class="header"><td><?php getWeekday(date('N')+6); ?></td></tr>
 				<tr class="header"><td> <?php echo date('d')+6 . ' ' . date('F');?> </td></tr>
-				<tr><td><?php getSkjarEinn(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+6, date("Y"))));?></td></tr>
+				<tr><td><?php getRuv(date('Y-m-d', mktime(0, 0, 0, date("m"), date("d")+6, date("Y"))));?></td></tr>
 			</table>
 		</td>
 	</tr>
