263fb5c73cfc41eb82802a639c56eac8c0adcbe3
beggaboo85 <beggaboo85@b1fc508c-c7c7-11de-8410-e5b1e99fc78e>
2009-11-22 13:36:32 +0000

    meira css styling

--- tvprogrammer/sMain.php
+++ tvprogrammer/sMain.php
@@ -1,6 +1,8 @@
-<input type="button" value="Ey&eth;a v&ouml;ldum &thorn;&aacute;ttum" onclick="eyda()"/>
+<input type="button" value="Ey&eth;a v&ouml;ldum &thorn;&aacute;ttum" class="btn" onclick="eyda()"/>
 <div class="ical"><a href="createCalender.php"><img src="img/ical.png" /></a></div>
 <div id="userCalender"></div>
+<div id="last"><a href="javascript:getLast()">Fyrri vika</a></div>
+<div id="next"><a href="javascript:getNext()">Næsta vika</a></div>
 <div id="calLoading"><br />
 <br /><br />
 Veri&eth; er a&eth; uppf&aelig;ra dagatali&eth; &thorn;itt, vinsamlegast hinkra&eth;u...<br />
--- tvprogrammer/sRegister.php
+++ tvprogrammer/sRegister.php
@@ -13,16 +13,16 @@
 	<td>Nafn:</td><td><input type="text" name="name" <?php if($fail) echo 'value=' . $_GET['name']; ?> /></td><td></td>
 </tr>
 <tr>
-	<td>E-mail:</td><td><input type="text" name="email" <?php if($fail) echo 'value=' . $_GET['email']; ?> /></td><td></td>
+	<td>E-mail:</td><td><input type="text" class="txt" name="email" <?php if($fail) echo 'value=' . $_GET['email']; ?> /></td><td></td>
 </tr>
 <tr>
-	<td>Notendanafn:</td><td><input type="text" name="username" <?php if($fail) echo 'value=' . $_GET['username']; ?> /></td><td><?php if($fail && strcmp($_GET['fail'],"2") == 0) echo 'Notendanafni&eth; er þegar til, veldu nýtt.' ?></td>
+	<td>Notendanafn:</td><td><input type="text" class="txt" name="username" <?php if($fail) echo 'value=' . $_GET['username']; ?> /></td><td><?php if($fail && strcmp($_GET['fail'],"2") == 0) echo 'Notendanafni&eth; er þegar til, veldu nýtt.' ?></td>
 </tr>
 <tr>
-	<td>Lykilor&eth;:</td><td><input type="password" name="password" /></td><td><?php if($fail && strcmp($_GET['fail'],"1") == 0) echo 'Lykilor&eth;in voru ekki eins reyndu aftur.' ?></td>
+	<td>Lykilor&eth;:</td><td><input type="password" class="txt" name="password" /></td><td><?php if($fail && strcmp($_GET['fail'],"1") == 0) echo 'Lykilor&eth;in voru ekki eins reyndu aftur.' ?></td>
 </tr>
 <tr>
-	<td>Lykilor&eth; aftur:</td><td><input type="password" name="password2" /></td><td></td>
+	<td>Lykilor&eth; aftur:</td><td><input type="password" class="txt" name="password2" /></td><td></td>
 </tr>
 <tr>
 	<td colspan="3"><input type="submit" value="Skrá sig" /></td>
